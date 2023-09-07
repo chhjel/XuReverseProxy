6605e74b8cd845fd86e8de5997ade9bc6b90f32d
Christian W <christian@lowzow.wang>
2023-09-07 23:53:44 +0200

    Use English culture for common placeholder values

--- XuReverseProxy.Core/Utils/PlaceholderUtils.cs
+++ XuReverseProxy.Core/Utils/PlaceholderUtils.cs
@@ -1,4 +1,5 @@
-﻿using XuReverseProxy.Core.Abstractions;
+﻿using System.Globalization;
+using XuReverseProxy.Core.Abstractions;
 
 namespace XuReverseProxy.Core.Utils;
 
@@ -20,17 +21,18 @@ public static class PlaceholderUtils
         return template;
     }
 
+    private static CultureInfo _placeholderLocale = CultureInfo.CreateSpecificCulture("en");
     public static string? ResolveCommonPlaceholders(string? template, Func<string?, string?>? transformer = null)
     {
         string mod(string val) => transformer?.Invoke(val) ?? val;
 
         return template
             ?.Replace("{{weekday}}", mod(DateTime.Now.DayOfWeek.ToString()), StringComparison.OrdinalIgnoreCase)
-            ?.Replace("{{monthName}}", mod(DateTime.Now.ToString("MMMM")), StringComparison.OrdinalIgnoreCase)
-            ?.Replace("{{dayOfMonth}}", mod(DateTime.Now.Day.ToString()), StringComparison.OrdinalIgnoreCase)
-            ?.Replace("{{hour}}", mod(DateTime.Now.Hour.ToString()), StringComparison.OrdinalIgnoreCase)
-            ?.Replace("{{minute}}", mod(DateTime.Now.Minute.ToString()), StringComparison.OrdinalIgnoreCase)
-            ?.Replace("{{month}}", mod(DateTime.Now.Month.ToString()), StringComparison.OrdinalIgnoreCase)
-            ?.Replace("{{year}}", mod(DateTime.Now.Year.ToString()), StringComparison.OrdinalIgnoreCase);
+            ?.Replace("{{monthName}}", mod(DateTime.Now.ToString("MMMM", _placeholderLocale)), StringComparison.OrdinalIgnoreCase)
+            ?.Replace("{{dayOfMonth}}", mod(DateTime.Now.Day.ToString(_placeholderLocale)), StringComparison.OrdinalIgnoreCase)
+            ?.Replace("{{hour}}", mod(DateTime.Now.Hour.ToString(_placeholderLocale)), StringComparison.OrdinalIgnoreCase)
+            ?.Replace("{{minute}}", mod(DateTime.Now.Minute.ToString(_placeholderLocale)), StringComparison.OrdinalIgnoreCase)
+            ?.Replace("{{month}}", mod(DateTime.Now.Month.ToString(_placeholderLocale)), StringComparison.OrdinalIgnoreCase)
+            ?.Replace("{{year}}", mod(DateTime.Now.Year.ToString(_placeholderLocale)), StringComparison.OrdinalIgnoreCase);
     }
 }
