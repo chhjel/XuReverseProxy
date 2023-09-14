e0d8098035c5852fbf0ff001a378c41b0cf783b8
Christian W <christian@lowzow.wang>
2023-09-14 21:44:08 +0200

    Make navmenu a tiny bit responsive

--- XuReverseProxy.Frontend/src/components/admin/AdminNavMenu.vue
+++ XuReverseProxy.Frontend/src/components/admin/AdminNavMenu.vue
@@ -13,23 +13,83 @@ export default class AdminNavMenu extends Vue {
 </script>
 
 <template>
-	<nav class="admin-nav">
-		<!-- <router-link to="/">Overview</router-link> | -->
-		<router-link to="/proxyconfigs">Proxies</router-link> |
-		<router-link to="/clients">Clients</router-link> |
-		<router-link to="/serverconfig">Server config</router-link> |
-		<router-link to="/jobs">Jobs</router-link> |
-		<router-link to="/admin-audit-log">Admin log</router-link> |
-		<router-link to="/client-audit-log">Client log</router-link> |
-		<a href="/auth/logout">[Logout]</a>
-	</nav>
+	<div>
+		<nav class="admin-nav-desktop">
+			<router-link to="/proxyconfigs">Proxies</router-link> |
+			<router-link to="/clients">Clients</router-link> |
+			<router-link to="/serverconfig">Server config</router-link> |
+			<router-link to="/jobs">Jobs</router-link> |
+			<router-link to="/admin-audit-log">Admin log</router-link> |
+			<router-link to="/client-audit-log">Client log</router-link>
+			<div class="spacer"></div>
+			<a href="/auth/logout" class="logout">[Logout]</a>
+		</nav>
+
+		<nav class="admin-nav-mobile">
+			<router-link to="/proxyconfigs">Proxies</router-link>
+			<router-link to="/clients">Clients</router-link>
+			<router-link to="/serverconfig">Server config</router-link>
+			<router-link to="/jobs">Jobs</router-link>
+			<router-link to="/admin-audit-log">Admin log</router-link>
+			<router-link to="/client-audit-log">Client log</router-link>
+			<a href="/auth/logout" class="logout">Logout</a>
+		</nav>
+	</div>
 </template>
 
 <style scoped lang="scss">
-.admin-nav {
+.admin-nav-desktop {
+	display: flex;
 	overflow-y: auto;
-    /* &__item {
+	padding-bottom: 5px;
+	@media (max-width: 599px) { display: none; }
 
-    } */
+	a {
+		margin-right: 5px;
+		white-space: nowrap;
+
+		&:not(:first-child) {
+			margin-left: 5px;
+		}
+	}
+
+    .router-link-active {
+		text-decoration: underline;
+	}
+
+	.logout {
+		color: var(--color--danger);
+	}
+}
+
+.admin-nav-mobile {
+	display: flex;
+	overflow-y: auto;
+	padding-bottom: 10px;
+	border-bottom: 3px solid var(--color--secondary-darken);
+	@media (min-width: 600px) { display: none; }
+
+	a {
+		margin-right: 5px;
+		white-space: nowrap;
+		padding: 20px 10px;
+		border: 1px solid var(--color--secondary);
+
+		&:not(:first-child) {
+			margin-left: 5px;
+		}
+	}
+
+    .router-link-active {
+		background-color: var(--color--secondary-darken);
+  		border-color: var(--color--secondary-darken);
+		text-decoration: underline;
+	}
+
+	.logout {
+		color: var(--color--danger-lighten);
+		background-color: #240d0d;
+		border-color: #4d1e1e;
+	}
 }
 </style>
