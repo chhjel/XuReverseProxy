adfdb0261010fd5db93c61eac8e9eff58be4fff6
Christian W <christian@lowzow.wang>
2023-09-14 09:33:11 +0200

    Add timing attack countermeasures

---
+++ XuReverseProxy.Core/Utils/AuthUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace XuReverseProxy.Core.Utils;
+
+public static class AuthUtils
+{
+    private static readonly Random _random = new();
+
+    // Delay a bit to make timing attacks harder
+    public static async Task RandomAuthDelay() => await Task.Delay(_random.Next(250, 750));
+}
