7a04549d76c401d1e85eb76fd9a6df06c72a9bd1
Christian W <christian@lowzow.wang>
2023-09-16 13:09:29 +0200

    Notify if memory logging is disabled on log page

--- XuReverseProxy.Frontend/src/pages/admin/ServerLogPage.vue
+++ XuReverseProxy.Frontend/src/pages/admin/ServerLogPage.vue
@@ -5,6 +5,7 @@ import { AdminPageFrontendModel } from "@generated/Models/Web/AdminPageFrontendM
 import ServerLogService from "@services/ServerLogService";
 import { LoggedEvent } from "@generated/Models/Core/LoggedEvent";
 import DateFormats from "@utils/DateFormats";
+import ServerConfigService from "@services/ServerConfigService";
 
 @Options({
 	components: {
@@ -16,9 +17,11 @@ export default class ServerLogPage extends Vue {
 	
     service: ServerLogService = new ServerLogService();
 	entries: Array<LoggedEvent> = [];
+	memoryLoggingEnabled: boolean | null = null;
 
 	async mounted() {
 		this.entries = await this.service.GetLogAsync();
+		this.memoryLoggingEnabled = await new ServerConfigService().IsConfigFlagEnabledAsync("EnableMemoryLogging");
 	}
 	
 	formatDate(raw: Date | string): string {
@@ -29,6 +32,7 @@ export default class ServerLogPage extends Vue {
 
 <template>
 	<div class="log-page">
+		<p v-if="memoryLoggingEnabled === false">- Memory logging is currently disabled -</p>
 		<div v-for="entry in entries">
 			<code>[{{ formatDate(entry.timestampUtc) }}] [{{ entry.logLevel }}] {{ entry.message }}</code>
 			<code v-if="entry.exception">{{ entry.exception }}</code>
