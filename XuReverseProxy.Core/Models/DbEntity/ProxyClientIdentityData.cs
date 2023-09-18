70c8d596051739b2665e3d06273881c2de9982b3
Christian W <christian@lowzow.wang>
2023-09-18 22:13:56 +0200

    Show in-progress OTP & manual approval challenge data on client page

--- XuReverseProxy.Core/Models/DbEntity/ProxyClientIdentityData.cs
+++ XuReverseProxy.Core/Models/DbEntity/ProxyClientIdentityData.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text.Json.Serialization;
 using XuReverseProxy.Core.Abstractions;
+using XuReverseProxy.Core.Attributes;
 
 namespace XuReverseProxy.Core.Models.DbEntity;
 
+[GenerateFrontendModel]
 public class ProxyClientIdentityData : IHasId
 {
     public Guid Id { get; set; }
