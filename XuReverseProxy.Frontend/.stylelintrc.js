efcafcc40e6b2c4f6ca9173cb9fee025cfb86b85
Christian W <christian@lowzow.wang>
2023-09-21 20:59:29 +0200

    Fix scss import rule

--- XuReverseProxy.Frontend/.stylelintrc.js
+++ XuReverseProxy.Frontend/.stylelintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   defaultSeverity: "warning",
   extends: ["stylelint-config-standard", "stylelint-config-sass-guidelines", "stylelint-prettier/recommended"],
+  rules: {
+    "import-notation": "string",
+  },
 };
