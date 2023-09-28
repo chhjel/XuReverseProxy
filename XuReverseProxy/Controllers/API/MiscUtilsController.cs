49a7fbeadbd806f6b78647d1efc063ac6e98c0b7
Christian W <christian@lowzow.wang>
2023-09-28 21:44:53 +0200

    Handle empty values better

--- XuReverseProxy/Controllers/API/MiscUtilsController.cs
+++ XuReverseProxy/Controllers/API/MiscUtilsController.cs
@@ -12,13 +12,17 @@ public class MiscUtilsController : Controller
 {
     [HttpPost("IsIPInCidrRange")]
     public bool IsIPInCidrRange([FromBody] TestCidrRangeRequestModel request)
-        => TKIPAddressUtils.IpMatchesOrIsWithinCidrRange(request.IP, request.IPCidr);
+    {
+        if (string.IsNullOrWhiteSpace(request.IP)) return false;
+        return TKIPAddressUtils.IpMatchesOrIsWithinCidrRange(request.IP, request.IPCidr);
+    }
     [GenerateFrontendModel]
     public record TestCidrRangeRequestModel(string IP, string IPCidr);
 
     [HttpPost("TestRegex")]
     public bool TestRegex([FromBody] TestRegexRequestModel request)
     {
+        if (request.Pattern == null) return false;
         var regex = new Regex(request.Pattern, RegexOptions.IgnoreCase);
         return regex.IsMatch(request.Input);
     }
